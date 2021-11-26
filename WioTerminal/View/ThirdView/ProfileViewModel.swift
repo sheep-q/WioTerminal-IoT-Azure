71c091118028423d68a5a70c4badd556db859acf
Quang Nguyeen <quangquangnguyen0501@gmail.com>
2021-11-26 16:58:14 +0700

    implement sarafiView, schedule and rule infomation

--- WioTerminal/View/ThirdView/ProfileViewModel.swift
+++ WioTerminal/View/ThirdView/ProfileViewModel.swift
@@ -12,7 +12,7 @@ class ProfileViewModel: ObservableObject {
     
     init() {
         self.profiles.append(ProfileModel(title: "Giảng viên hướng dẫn", image: "coYen", text: "PGS.TS Phạm Thị Ngọc Yến", description: "Viện Điện - Đo lường, Tin học và Công nghiệp"))
-        self.profiles.append(ProfileModel(title: "Giảng viên hướng dẫn", image: "thayTung", text: "TS.Nguyễn Việt Tùng", description: "Viện Phó Viện Mica"))
+        self.profiles.append(ProfileModel(title: "Giảng viên hướng dẫn", image: "thayTung", text: "TS.Nguyễn Việt Tùng", description: "Viện nghiên cứu quốc tế và truyền thông Mica"))
         self.profiles.append(ProfileModel(title: "Sinh viên thực hiện", image: "sv", text: "Nguyễn Văn Quang - 20174138", description: "KTĐK & TĐH 06"))
     }
 }
